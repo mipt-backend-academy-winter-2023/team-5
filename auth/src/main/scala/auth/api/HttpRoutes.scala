a8c1d26bb6d83eef2634dc78131a9abed3f40f8d
Timofey Khoruzhii <timosha1113@gmail.com>
2023-05-02 23:44:52 +0300

    Solve

--- auth/src/main/scala/auth/api/HttpRoutes.scala
+++ auth/src/main/scala/auth/api/HttpRoutes.scala
@@ -1,7 +1,40 @@
 package auth.api
 
+import zio.{ZIO, Task}
 import zio.http._
+import zio.http.model.{Method, Status}
+import zio.json._
+
+case class UserCredentials(login: String, password: String)
+object UserCredentials {
+  implicit val decoder: JsonDecoder[UserCredentials] = DeriveJsonDecoder.gen[UserCredentials]
+}
+
+case class AuthorizationToken(token: String)
+object AuthorizationToken {
+  implicit val encoder: JsonEncoder[AuthorizationToken] = DeriveJsonEncoder.gen[AuthorizationToken]
+}
 
 object HttpRoutes {
-  val app: HttpApp[Any, Response] = ???
+  val app: HttpApp[Any, Response] =
+    Http.collectZIO[Request] {
+      case req @ Method.POST -> !! / "auth" / "login" => {
+        req.body.asString.map(body =>
+          body.fromJson[UserCredentials] match {
+          case Left(_) => Response.status(Status.Forbidden)
+          case Right(data) => authenticate(data)
+        })
+      }.orElseFail(Response.status(Status.BadRequest))
+      case req@Method.POST -> !! / "auth" / "register" => {
+        req.body.asString.map(body =>
+          body.fromJson[UserCredentials] match {
+            case Left(_) => Response.status(Status.Forbidden)
+            case Right(data) => Response.status(Status.Ok)
+          })
+      }.orElseFail(Response.status(Status.BadRequest))
+    }
+
+  private def authenticate(credentials: UserCredentials): Response = {
+    Response.json(AuthorizationToken("dummy-token").toJson)
+  }
 }
--- routing/src/main/scala/routing/api/HttpRoutes.scala
+++ routing/src/main/scala/routing/api/HttpRoutes.scala
@@ -1,7 +1,41 @@
 package routing.api
 
+import zio.{Task, ZIO}
 import zio.http._
+import zio.http.model.{Method, Status}
+import zio.json._
+
+case class AuthorizationToken(token: String)
+object AuthorizationToken {
+  implicit val encoder: JsonEncoder[AuthorizationToken] = DeriveJsonEncoder.gen[AuthorizationToken]
+}
+
+case class IdMapPoint(value: String)
+object IdMapPoint {
+  implicit val encoder: JsonEncoder[IdMapPoint] = DeriveJsonEncoder.gen[IdMapPoint]
+  implicit val decoder: JsonDecoder[IdMapPoint] = DeriveJsonDecoder.gen[IdMapPoint]
+}
+
+case class MapPoint(id: IdMapPoint, name: String)
+object MapPoint {
+  implicit val encoder: JsonEncoder[MapPoint] = DeriveJsonEncoder.gen[MapPoint]
+  implicit val decoder: JsonDecoder[MapPoint] = DeriveJsonDecoder.gen[MapPoint]
+}
 
 object HttpRoutes {
-  val app: HttpApp[Any, Response] = ???
+  val app: HttpApp[Any, Response] =
+    Http.collectZIO[Request] {
+      // Ключиков нет в ямле, делать не буду ибо волк заходит в бар
+      case req @ Method.POST -> !! / "route" / "search" => {
+        req.body.asString.map(body =>
+          body.fromJson[List[IdMapPoint]] match {
+            case Left(_) => Response.status(Status.Forbidden)
+            case Right(data) => route(data)
+          })
+      }.orElseFail(Response.status(Status.BadRequest))
+    }
+
+  private def route(credentials: List[IdMapPoint]): Response = {
+    Response.json(List(MapPoint(IdMapPoint("Тутдым"), "сюдым"), MapPoint(IdMapPoint("чота там"), "кому то там")).toJson)
+  }
 }
