001ce95ab54216efae6bd9da590989c165dbae69
Timofey Khoruzhii <timosha1113@gmail.com>
2023-05-24 06:40:55 +0300

    fix password hash in migration

--- repository/src/main/resources/db/migration/V1__user_table.sql
+++ repository/src/main/resources/db/migration/V1__user_table.sql
@@ -5,4 +5,4 @@ create table users
     password_hash varchar
 );
 
-insert into users (username, password_hash) values ('timk', 'verystrongpassword');
+insert into users (username, password_hash) values ('timk', 'cebf2cb7f8d7c263837cf63e10cecb98a0560c181d34e6c4bdab3f28e619cabc'); -- password: verystrongpassword
