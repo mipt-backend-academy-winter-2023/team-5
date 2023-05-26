b2f6fd0f35dbc9bfe116557e04cde0fcb314e3bf
Timofey Khoruzhii <timosha1113@gmail.com>
2023-05-26 05:33:53 +0300

    fix

--- repository/src/main/resources/db/migration/V1__user_table.sql
+++ repository/src/main/resources/db/migration/V1__user_table.sql
@@ -1,8 +1,8 @@
 create table users
 (
-    id serial,
-    username varchar,
-    password_hash varchar
+    id serial primary key,
+    username varchar not null primary key,
+    password_hash varchar not null
 );
 
 insert into users (username, password_hash) values ('timk', 'cebf2cb7f8d7c263837cf63e10cecb98a0560c181d34e6c4bdab3f28e619cabc'); -- password: verystrongpassword
