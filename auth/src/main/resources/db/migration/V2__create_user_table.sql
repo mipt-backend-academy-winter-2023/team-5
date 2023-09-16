1a73878ab063c45ccc68bb5269648f9bca100516
Vladimir Polushin <vovapolu@gmail.com>
2023-09-16 12:06:43 +0300

    Revert "Mustafin DB"

--- auth/src/main/resources/db/migration/V2__create_user_table.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE "user"
-(
-    "login" VARCHAR NOT NULL PRIMARY KEY,
-    "password"  VARCHAR NOT NULL
-);
-
-INSERT INTO "user" (login, password)
-VALUES  ('artem', '1234'),
-        ('test', 'test');
