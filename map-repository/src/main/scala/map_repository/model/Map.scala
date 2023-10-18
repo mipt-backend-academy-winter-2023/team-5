1fabcddc1f23f873a1a86bcd94de817e9df62046
Timofey Khoruzhii <timosha1113@gmail.com>
2023-10-18 21:29:55 +0300

    fix routing

--- map-repository/src/main/scala/map_repository/model/Map.scala
+++ map-repository/src/main/scala/map_repository/model/Map.scala
@@ -1,7 +1,15 @@
 package map_repository.model
 
+import zio.json.{DeriveJsonDecoder, DeriveJsonEncoder, JsonDecoder, JsonEncoder}
+
 case class Street(id: Int, name: String)
 case class Point(id: Int, name: String, x: Float, y: Float)
+object Point {
+  implicit val decoder: JsonDecoder[Point] =
+    DeriveJsonDecoder.gen[Point]
+  implicit val encoder: JsonEncoder[Point] =
+    DeriveJsonEncoder.gen[Point]
+}
 case class Edge(
     id: Int,
     pointFrom: Int,
